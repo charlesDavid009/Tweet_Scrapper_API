99d10341a11a2b4ceb2c55c7cff6e2ea61dae3a6
Zues D'ghost <zuesghost1@gmail.com>
2021-07-12 11:54:57 +0100

    ADDED ADMIN PRIVILEDGES AND REGISTRATION TO SITE

--- python_tips_api/contributions/admin.py
+++ python_tips_api/contributions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.sites.register(Tips)
+admin.sites.register(TweetsPythonTips)
+admin.sites.register(TipsLikes) #i could move this into a tabular inline with tips but lets leave it like this for now
