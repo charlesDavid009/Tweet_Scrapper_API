e9fbd8ce53003e7aa6313fa8c86299baf8729e44
Zues D'ghost <zuesghost1@gmail.com>
2021-07-12 12:17:11 +0100

    ADDED JSON RENDERERS AND UTF SECURTIY

--- python_tips_api/authentication/urls.py
+++ python_tips_api/authentication/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view(), name="logout"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('request-reset-email/', RequestPasswordResetEmail.as_view(), name="request-reset-email"),
-    path('password-reset/<uidb64>/<token>/',PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
-    path('password-reset-complete', SetNewPasswordAPIView.as_view(), name='password-reset-complete')
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
 ]
-
